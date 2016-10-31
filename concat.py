c2c6ba6465428bdb8ab080d645cfa5c62f2ce0bb
GuyKha <guykhag@gmail.com>
2016-10-31 17:40:03 -0400

    Starting migration
    First display of gui

--- concat.py
+++ concat.py
@@ -1,59 +1,22 @@
-#!/usr/bin/python
-import sys
-import os
-import zipfile
-import tempfile
-import shutil
+from gooey import Gooey, GooeyParser
 
-from os import listdir
-from os.path import isfile, join, basename
+@Gooey(advanced=True,  
+       program_name='name',       # Defaults to script name
+       program_description = 'concatentor',       # Defaults to ArgParse Description
+       default_size=(500, 350),   # starting size of the GUI
+       required_cols=1,           # number of columns in the "Required" section
+       optional_cols=1
+    )   
+def main():
+    """ Use GooeyParser to build up the arguments we will use in our script
+    Save the arguments in a default json file so that we can retrieve them
+    every time we run the script.
+    """
+    parser = GooeyParser(description='Concatenation app')
+    parser.add_argument('inputDir',action='store',widget='DirChooser',help="The directory that contains the files to concatenate")
+    parser.add_argument('compress',action='store_true',widget='CheckBox',help='Compress')
+    args = parser.parse_args()
 
-if (len(sys.argv) < 4):
-    print("Invalid usage at least two files are expected\nusage: concat.py <FILE_1> <FILE_2> ... <OUTPUT_NAME>")
-    sys.exit(0)
-
-# intial variables, creating temporary dir and saving output name
-temp_dir = tempfile.mkdtemp()
-outputName = sys.argv[len(sys.argv) -1]
-currentDirectory = os.path.dirname(os.path.abspath(__file__))
-common_extension = None # will save the extension of the output file
-
-try:
-    # running over all of the arguments and extracting
-    for i in range(1, len(sys.argv)-1):
-        if (os.path.isfile(sys.argv[i])):
-            # getting file extension and checking it's validity
-            extension = os.path.splitext(sys.argv[i])[1]
-            if (extension == ".zip"):
-                # opening zip file
-                zip_ref = zipfile.ZipFile(sys.argv[i], 'r')
-                print("Extracting ",  sys.argv[i])
-                # getting zip file members 
-                members = zip_ref.namelist()
-                #print("Extracted: " + members)
-                zip_ref.extractall(temp_dir, members)
-                zip_ref.close()
-            else:
-                print("Invalid file "+  sys.argv[i] +  " skipping")
-        else:
-            print("File ",  sys.argv[i],  " doesn't exist")
-    
-    # running over all of the files in the temp dir and concatenating files saving the in the original directory
-    files = [f for f in listdir(temp_dir) if isfile(join(temp_dir, f))]
-    # getting the extension
-    common_extension =  os.path.splitext(temp_dir+"/"+ files[0])[1]
-    with open(temp_dir+"/"+ outputName + common_extension, 'wb') as outfile:
-        for filename in files:
-            with open(temp_dir+"/"+ filename, 'rb') as readfile:
-                shutil.copyfileobj(readfile, outfile)
-    
-    #compressing output file
-    zout = zipfile.ZipFile(outputName + ".zip", "w", zipfile.ZIP_DEFLATED) 
-    zout.write(temp_dir+"/"+ outputName + common_extension,  basename(temp_dir+"/"+ temp_dir+"/"+ outputName + common_extension))
-    zout.close()
-# deleting temporary dir finally
-finally:
-    shutil.rmtree(temp_dir)
-        
-    
-
+# define main as start module
+if __name__ == "__main__":
+    main()
